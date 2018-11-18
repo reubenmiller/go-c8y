910060a7628b350be182cc2abb2189dd7fda95b0
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2018-11-18 14:41:07 +0100

    Loading realtime authentication client from an application.properties file

--- c8y_test/realtime_test.go
+++ c8y_test/realtime_test.go
@@ -1,29 +1,16 @@
 package c8y_test
 
 import (
-	"fmt"
-	"os"
 	"testing"
-
-	c8y "github.com/reubenmiller/go-c8y"
 )
 
 func TestRealtimeClient(t *testing.T) {
-	host := os.Getenv("C8Y_HOST")
-	tenant := os.Getenv("C8Y_TENANT")
-	username := os.Getenv("C8Y_USERNAME")
-	password := os.Getenv("C8Y_PASSWORD")
+	client := createTestClient()
+	realtime := client.Realtime
 
-	fmt.Printf("Host %s, Tenant %s, Username %s, Password %s", host, tenant, username, password)
+	err := realtime.Connect()
 
-	if tenant == "" || username == "" || password == "" {
-		t.Errorf("Missing Cumulocity C8Y_TENANT, C8Y_USERNAME, C8Y_PASSWORD environement variable which are required for this test")
-	}
-	client := c8y.NewRealtimeClient(host, nil, tenant, username, password)
-
-	err := client.Connect()
-
-	err = client.WaitForConnection()
+	err = realtime.WaitForConnection()
 
 	if err != nil {
 		t.Errorf("Unknown error")
