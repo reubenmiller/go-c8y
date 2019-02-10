294fba5c065cd9c21a3388cb6f0367324c7a372e
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2019-02-10 16:43:01 +0100

    Removing vendor files in microservice package

--- microservice/vendor/github.com/reubenmiller/go-c8y/device.go
+++
@@ -1,7 +0,0 @@
-package c8y
-
-// Source represents a source reference
-type Source struct {
-	ID   string `json:"id,omitempty"`
-	Self string `json:"self,omitempty"`
-}
--- microservice/vendor/github.com/reubenmiller/go-c8y/paginationOptions.go
+++
@@ -1,9 +0,0 @@
-package c8y
-
-// PaginationOptions is the cumulocity pagination options
-type PaginationOptions struct {
-	// Pagesize of results to return in one request
-	PageSize int `url:"pageSize,omitempty"`
-
-	WithTotalPages bool `url:"withTotalPages,omitempty"`
-}
