a67e6161667de23a4082b4445d3b995ec905d03d
Reuben Miller <reuben.d.miller@gmail.com>
2019-03-03 11:12:07 +0100

    Added test for measurement subscriptions

--- c8y_test/realtime_test.go
+++ c8y_test/realtime_test.go
@@ -163,3 +163,98 @@ func TestRealtimeSubscriptions_SubscribeToOperations(t *testing.T) {
 		}
 	}
 }
+
+func TestRealtimeSubscriptions_SubscribeToMeasurements(t *testing.T) {
+	device, err := createTestDevice()
+
+	if err != nil {
+		t.Errorf("Device should exist. wanted nil, got %s", err)
+	}
+
+	client := createTestClient()
+	realtime := client.Realtime
+
+	go func() {
+		realtime.Connect()
+	}()
+
+	err = realtime.WaitForConnection()
+
+	if err != nil {
+		t.Errorf("Unknown error")
+	}
+
+	ch := make(chan *c8y.Message)
+	timerChan := time.NewTimer(time.Second * 5).C
+
+	realtime.Subscribe("/measurements/"+device.ID, ch)
+
+	// Create a dummy measurement
+	sendMeasurement := func(value float64) {
+		meas, err := c8y.NewSimpleMeasurementRepresentation(
+			c8y.SimpleMeasurementOptions{
+				SourceID:            device.ID,
+				Type:                "testMeasurement1",
+				ValueFragmentType:   "c8y_Test",
+				ValueFragmentSeries: "Measurement1",
+				Value:               value,
+				Unit:                "Counter",
+			},
+		)
+
+		if err != nil {
+			t.Errorf("Failed to create measurement. %s", err)
+		}
+		_, _, err = client.Measurement.Create(
+			context.Background(),
+			*meas,
+		)
+		if err != nil {
+			t.Errorf("Failed to create measurement. %s", err)
+		}
+	}
+
+	expectedMeasValue := 1.0
+	sendMeasurement(expectedMeasValue)
+	sendMeasurement(expectedMeasValue)
+
+	defer func() {
+		close(ch)
+		realtime.Close()
+	}()
+
+	msgCount := 0
+
+	for {
+		select {
+		case msg := <-ch:
+			msgCount++
+
+			if msg.Payload.RealtimeAction != "CREATE" {
+				t.Errorf("Unexpected realtime action type. wanted: CREATE, got: %s", msg.Payload.RealtimeAction)
+			}
+
+			measValue := msg.Payload.Item.Get("c8y_Test.Measurement1.value").Float()
+			if measValue != expectedMeasValue {
+				t.Errorf("Unexpected measurement value. wanted: %f, got: %f", expectedMeasValue, measValue)
+			}
+
+			deviceId := msg.Payload.Item.Get("source.id").String()
+			if deviceId != device.ID {
+				t.Errorf("Unexpected device id in operation. wanted: %s, got: %s", device.ID, deviceId)
+			}
+
+			log.Printf("Received notification")
+			log.Printf("ws: [frame]: Channel: %s, %s\n", msg.Channel, string(msg.Payload.Item.Raw))
+
+		case <-timerChan:
+			realtime.UnsubscribeAll()
+
+			if msgCount != 2 {
+				t.Errorf("Unexpected message count. wanted: 2, got: %d", msgCount)
+			}
+			realtime.Close()
+			return
+		}
+	}
+}
