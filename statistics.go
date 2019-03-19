c3e36ecb5155bd75b9855676a8506eb466db0c43
Reuben Miller <reuben.d.miller@gmail.com>
2019-03-19 21:21:16 +0100

    Removed unused function

--- statistics.go
+++ statistics.go
@@ -1,7 +1,5 @@
 package c8y
 
-import "sync"
-
 // Statistics statistics about the returned rest response
 type Statistics struct {
 	CurrentPage *int `json:"currentPage"`
@@ -14,24 +12,4 @@ type BaseResponse struct {
 	Next       *string     `json:"next"`
 	Self       *string     `json:"self"`
 	Statistics *Statistics `json:"statistics"`
-}
-
-// GetAll returns all of the objects as detailed in the pagination object
-func (resp *BaseResponse) GetAll() (interface{}, error) {
-
-	var wg sync.WaitGroup
-	totalPages := 10
-
-	for i := 2; i < totalPages; i++ {
-		wg.Add(1)
-
-		// Send parallel calls to collect all of the devices
-		go func() {
-			defer wg.Done()
-		}()
-	}
-
-	wg.Wait()
-
-	return resp.Next, nil
 }
