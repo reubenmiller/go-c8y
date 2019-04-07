eb34f52b620d3a78a2ae5fa38ca00919bb3159ce
Reuben Miller <reuben.d.miller@gmail.com>
2019-04-07 14:49:40 +0200

    Renamed CustomOperation struct to OperationBuilder and added tests

--- pkg/c8y/operationRepresentation.go
+++ pkg/c8y/operationRepresentation.go
@@ -1,9 +1,6 @@
 package c8y
 
 import (
-	"encoding/json"
-	"errors"
-
 	"github.com/tidwall/gjson"
 )
 
@@ -20,49 +17,4 @@ type Operation struct {
 	FailureReason string     `json:"failureReason,omitempty"`
 
 	Item gjson.Result `json:"-"`
-}
-
-// CustomOperation is a generic operation representation that can be used to build custom operations with a free format
-type CustomOperation struct {
-	data map[string]interface{}
-}
-
-// NewCustomOperation returns a new Custom Operation with the specified device id
-func NewCustomOperation(deviceID string) *CustomOperation {
-	op := &CustomOperation{
-		data: map[string]interface{}{},
-	}
-	// op.data = make(map[string]interface{})
-	op.SetDeviceID(deviceID)
-	return op
-}
-
-// MarshalJSON returns the given operation in json format
-func (o CustomOperation) MarshalJSON() ([]byte, error) {
-	return json.Marshal(o.data)
-}
-
-// DeviceID returns the device id of the operation
-func (o CustomOperation) DeviceID() string {
-	return o.data["deviceId"].(string)
-}
-
-// SetDeviceID sets the device id for the custom operation
-func (o *CustomOperation) SetDeviceID(ID string) *CustomOperation {
-	o.data["deviceId"] = ID
-	return o
-}
-
-// Set sets the name property with the given value
-func (o *CustomOperation) Set(name string, value interface{}) *CustomOperation {
-	o.data[name] = value
-	return o
-}
-
-// Get returns the given property value. If the property does not exist, then the error will have a non nil value
-func (o CustomOperation) Get(name string) (interface{}, error) {
-	if v, ok := o.data[name]; ok {
-		return v, nil
-	}
-	return nil, errors.New("Unknown property")
 }
