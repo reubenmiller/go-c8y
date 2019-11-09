925c5e38810ee88f95f9818e6c93634958765871
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2019-11-09 20:22:09 +0100

    Adding map builder to create nested maps with dot notation

---
+++ pkg/mapbuilder/mapbuilder.go
@@ -0,0 +1,66 @@
+package mapbuilder
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+const (
+	Separator = "."
+)
+
+func NewMapBuilder() *MapBuilder {
+	return &MapBuilder{
+		body: make(map[string]interface{}),
+	}
+}
+
+func NewMapBuilderWithInit(body map[string]interface{}) *MapBuilder {
+	return &MapBuilder{
+		body: body,
+	}
+}
+
+// MapBuilder creates body builder
+type MapBuilder struct {
+	body map[string]interface{}
+}
+
+// SetMap sets a new map to the body. This will remove any existing values in the body
+func (b *MapBuilder) SetMap(body map[string]interface{}) {
+	b.body = body
+}
+
+// GetMap returns the body as a map[string]interface{}
+func (b MapBuilder) GetMap() map[string]interface{} {
+	return b.body
+}
+
+// MarshalJSON returns the body as json
+func (b MapBuilder) MarshalJSON() ([]byte, error) {
+	return json.Marshal(b.body)
+}
+
+// Set sets a value to a give dot notation path
+func (b *MapBuilder) Set(path string, value interface{}) error {
+	keys := strings.Split(path, Separator)
+
+	currentMap := b.body
+
+	lastIndex := len(keys) - 1
+
+	for i, key := range keys {
+		if key != "" {
+			if i != lastIndex {
+				if _, ok := currentMap[key]; !ok {
+					currentMap[key] = make(map[string]interface{})
+				}
+				currentMap = currentMap[key].(map[string]interface{})
+			} else {
+				currentMap[key] = value
+			}
+		}
+	}
+
+	return nil
+}
