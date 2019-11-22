91f4911d9875f4f9ce463c9959c1a0cf17a6a0c0
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2019-11-22 18:38:29 +0100

    Updating template to use new logger

--- internal/api/New-C8yApiGoCommand.ps1
+++ internal/api/New-C8yApiGoCommand.ps1
@@ -137,7 +137,6 @@ import (
     "context"
     "encoding/json"
     "fmt"
-    "log"
     "net/url"
 
     "github.com/fatih/color"
@@ -215,7 +214,7 @@ func (n *${Name}Cmd) do${NameCamel}(method string, path string, query string, bo
 
     if resp != nil && resp.JSONData != nil {
         // estimate size based on utf8 encoding. 1 char is 1 byte
-	    log.Printf("Response Length: %0.1fKB", float64(len(*resp.JSONData)*1)/1024)
+	    Logger.Printf("Response Length: %0.1fKB", float64(len(*resp.JSONData)*1)/1024)
 
         var responseText []byte
 
