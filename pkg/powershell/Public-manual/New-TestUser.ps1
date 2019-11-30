110945c27033faac286c6ad778233db9c24b204e
reuben.d.miller@gmail.com <reuben.d.miller@gmail.com>
2019-11-30 17:45:47 +0100

    Setting default tenant name

---
+++ pkg/powershell/Public-manual/New-TestUser.ps1
@@ -0,0 +1,19 @@
+Function New-TestUser {
+<#
+.SYNOPSIS
+Create a new test user
+#>
+    [cmdletbinding()]
+    Param(
+        # Name of the username. A random postfix will be added to it to make it unique
+        [Parameter(
+            Mandatory = $false,
+            Position = 0
+        )]
+        [string] $Name = "testuser"
+    )
+
+    $Username = New-RandomString -Prefix "${Name}_"
+
+    PSC8y\New-User -UserName $Username -Password (New-RandomString)
+}
