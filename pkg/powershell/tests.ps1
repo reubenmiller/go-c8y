d2efd0054cce53b0062ad74ce2d9d629a2940300
Reuben Miller <reuben.d.miller@gmail.com>
2019-12-21 13:14:45 +0100

    Installing pester if not available

--- pkg/powershell/tests.ps1
+++ pkg/powershell/tests.ps1
@@ -5,10 +5,15 @@ Param(
 $OldConfirmPreference = $global:ConfirmPreference
 $global:ConfirmPreference = "None"
 
+if (!(Get-Module "Pester")) {
+    Install-Module "Pester" -MinimumVersion "4.0.0"
+    Import-Module "Pester" -MinimumVersion "4.0.0"
+}
+
 if ($Coverage) {
     $output = Invoke-Pester -Script $PSScriptRoot/Tests -CodeCoverage $PSScriptRoot/Public/* -CodeCoverageOutputFile "$PSScriptRoot/PSC8y.coverage.xml" -PassThru
 } else {
-    Invoke-Pester -Script $PSScriptRoot/Tests
+    Invoke-Pester -Script $PSScriptRoot/Tests -OutputFile "report/report.xml"
 }
 
 
