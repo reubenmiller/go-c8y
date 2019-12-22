421bcd68667893922ef05c3cb7425160d6b40ff5
Reuben Miller <reuben.d.miller@gmail.com>
2019-12-22 17:34:24 +0100

    Adding nuget publish script (not used)

--- scripts/build-powershell/publish.ps1
+++ scripts/build-powershell/publish.ps1
@@ -1,30 +1,44 @@
+[cmdletbinding()]
+Param()
 $ErrorActionPreference = 'Stop'
+
+Import-Module PowershellGet -MinimumVersion "2.0.0"
 
 try {
 	## Don't upload the build scripts and appveyor.yml to PowerShell Gallery
-	$tempmoduleFolderPath = "$env:Temp\PSc8y"
-	$null = mkdir $tempmoduleFolderPath
+	$tempName = New-TemporaryFile
+	Remove-Item $tempName -Force
+	$tempmoduleFolderPath = Join-Path $tempName -ChildPath "PSc8y"
+	$null = New-Item $tempmoduleFolderPath -ItemType Directory
+	Write-Host "Temp folder: $tempmoduleFolderPath"
 
 	## Remove all of the files/folders to exclude out of the main folder
 	$excludeFromPublish = @(
-		'PSc8y\\appveyor\.yml'
-		'PSc8y\\\.git'
-		'PSc8y\\\.nuspec'
-		'PSc8y\\README\.md'
-		'PSc8y\\TestUsers\.csv'
-		'PSc8y\\TestResults\.xml'
-		'PSc8y\\TestingCode\.ps1'
-
+		'PSc8y[\\\/]appveyor\.yml'
+		'PSc8y[\\\/]\.git'
+		'PSc8y[\\\/]\.nuspec'
+		'PSc8y[\\\/]README\.md'
+		'PSc8y[\\\/]CHANGELOG\.md'
+		'PSc8y[\\\/]tests\.ps1'
+		'PSc8y[\\\/]Tests'
 	)
+
+	$ProjectDir = Resolve-Path "$PSScriptRoot/../../tools/PSc8y"
+
+	Copy-Item -Path "$ProjectDir/*" -Destination "$tempmoduleFolderPath/" -Recurse
+
 	$exclude = $excludeFromPublish -join '|'
-	Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER -Recurse | Where-Object { $_.FullName -match $exclude } | Remove-Item -Force -Recurse
+	Get-ChildItem -Path $tempmoduleFolderPath -Recurse `
+		| Where-Object { $_.FullName -match $exclude } `
+		| Remove-Item -Force -Recurse
+
 
 	## Publish module to PowerShell Gallery
 	$publishParams = @{
-		Path        = $env:APPVEYOR_BUILD_FOLDER
+		Path        = $tempmoduleFolderPath
 		NuGetApiKey = $env:nuget_apikey
 	}
-	Publish-PMModule @publishParams
+	Publish-Module @publishParams
 
 } catch {
 	Write-Error -Message $_.Exception.Message
