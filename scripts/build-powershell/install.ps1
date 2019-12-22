a884aa25e0c765b1a2b31ed8980a32737a5082cd
Reuben Miller <reuben.d.miller@gmail.com>
2019-12-22 11:22:50 +0100

    Skiping nuget provider on linux and mac

--- scripts/build-powershell/install.ps1
+++ scripts/build-powershell/install.ps1
@@ -4,21 +4,25 @@ $ErrorActionPreference = 'Stop'
 
 try {
 
-	$provParams = @{
-		Name           = 'NuGet'
-		MinimumVersion = '2.8.5.208'
-		Force          = $true
-	}
+	if (!($IsLinux -or $IsMacOS)) {
+		$provParams = @{
+			Name           = 'NuGet'
+			MinimumVersion = '2.8.5.208'
+			Force          = $true
+		}
 
-	$null = Install-PackageProvider @provParams
-	$null = Import-PackageProvider @provParams
+		$null = Install-PackageProvider @provParams
+		$null = Import-PackageProvider @provParams
+	}
 
 	$requiredModules = @('Pester', 'PSScriptAnalyzer')
 	foreach ($m in $requiredModules) {
 		Write-Host "Installing [$($m)] module..."
 		Install-Module -Name $m -Force -Confirm:$false
+
 		Write-Host "Removing [$($m)] module from current session..."
 		Remove-Module -Name $m -Force -ErrorAction Ignore
+
 		Write-Host "Importing [$($m)] module into current session..."
 		Import-Module -Name $m
 	}
