5cd9d4d8b5dc267dbc4bd51fb0724f1bd0128a04
Reuben Miller <reuben.d.miller@gmail.com>
2019-12-22 23:05:59 +0100

    Updating auto gen tests

--- tools/PSc8y/Tests/Get-AllTenantUsageSummaryStatistics.auto.Tests.ps1
+++ tools/PSc8y/Tests/Get-AllTenantUsageSummaryStatistics.auto.Tests.ps1
@@ -5,17 +5,17 @@ Describe -Name "Get-AllTenantUsageSummaryStatistics" {
 
     }
 
-    It "Get tenant summary statistics for all tenants" {
+    It -Skip "Get tenant summary statistics for all tenants" {
         $Response = PSc8y\Get-AllTenantUsageSummaryStatistics
         $LASTEXITCODE | Should -Be 0
         $Response | Should -Not -BeNullOrEmpty
     }
-    It "Get tenant summary statistics collection for the last 30 days" {
+    It -Skip "Get tenant summary statistics collection for the last 30 days" {
         $Response = PSc8y\Get-AllTenantUsageSummaryStatistics -DateFrom "-30d"
         $LASTEXITCODE | Should -Be 0
         $Response | Should -Not -BeNullOrEmpty
     }
-    It "Get tenant summary statistics collection for the last 10 days, only return until the last 9 days" {
+    It -Skip "Get tenant summary statistics collection for the last 10 days, only return until the last 9 days" {
         $Response = PSc8y\Get-AllTenantUsageSummaryStatistics -DateFrom "-10d" -DateTo "-9d"
         $LASTEXITCODE | Should -Be 0
         $Response | Should -Not -BeNullOrEmpty
--- tools/PSc8y/Tests/Update-TenantOptionEditable.auto.Tests.ps1
+++ tools/PSc8y/Tests/Update-TenantOptionEditable.auto.Tests.ps1
@@ -6,7 +6,7 @@ Describe -Name "Update-TenantOptionEditable" {
 
     }
 
-    It "Update editable property for an existing tenant option" {
+    It -Skip "Update editable property for an existing tenant option" {
         $Response = PSc8y\Update-TenantOptionEditable -Category "c8y_cli_tests" -Key "option8" -Editable "true"
         $LASTEXITCODE | Should -Be 0
         $Response | Should -Not -BeNullOrEmpty
