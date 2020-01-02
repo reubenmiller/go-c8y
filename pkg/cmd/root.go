a2884fc3cbda832956d4d184a6fa3b20c0db4a6f
Reuben Miller <reuben.d.miller@gmail.com>
2020-01-02 14:56:16 +0100

    updating c8y command description

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -47,12 +47,7 @@ func newBaseCmd(cmd *cobra.Command) *baseCmd {
 var rootCmd = &cobra.Command{
 	Use:   "c8y",
 	Short: "Cumulocity command line interface",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
-	},
+	Long:  `A command line interface to interact with Cumulocity REST API. Ideal for quick prototyping, exploring the REST API and for Platform maintainers/power users`,
 }
 
 var (
