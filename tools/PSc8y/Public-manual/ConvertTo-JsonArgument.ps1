e935804a1abc68144d9af7449a916d8f39c00cc7
y509540 <y509540@eur.ad.sag@SAG-FJNRYY2-U.EUR.AD.SAG>
2020-01-12 18:24:56 +0100

    Allowing input as json string

--- tools/PSc8y/Public-manual/ConvertTo-JsonArgument.ps1
+++ tools/PSc8y/Public-manual/ConvertTo-JsonArgument.ps1
@@ -7,7 +7,15 @@ Function ConvertTo-JsonArgument {
         )]
         [object] $Data
     )
-    $strArg = "{0}" -f ((ConvertTo-Json $Data -Compress) -replace '"', '\"')
+
+    if ($Data -is [string]) {
+        # If string, then validate if json was provided
+        $DataObj = (ConvertFrom-Json $Data)
+    } else {
+        $DataObj = $Data
+    }
+
+    $strArg = "{0}" -f ((ConvertTo-Json $DataObj -Compress) -replace '"', '\"')
 
     # Replace space with unicode char, as space can have console parsing problems
     $strArg = $strArg -replace " ", "\u0020"
