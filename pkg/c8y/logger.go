bcafc4eec219cc41963ac1374e63c1094c2fb0fb
Reuben Miller <reuben.miller@softwareag.com>
2020-09-23 23:14:57 +0000

    adding in missing logger file (excluded due to gitignore rule)

---
+++ pkg/c8y/logger.go
@@ -0,0 +1,17 @@
+package c8y
+
+import (
+	"github.com/reubenmiller/go-c8y/pkg/logger"
+)
+
+// Logger used within the c8y client
+var Logger *logger.Logger
+
+func init() {
+	Logger = logger.NewLogger("c8y")
+}
+
+// SilenceLogger causes all log messages to be hidden
+func SilenceLogger() {
+	Logger = logger.NewDummyLogger("c8y")
+}
