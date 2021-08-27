8ca6e5830e3f1cfca9afc35fc3e0ca5898b16c0a
reubenmiller <reuben.d.miller@gmail.com>
2021-08-27 19:38:17 +0000

    fixing firmware version query

--- pkg/c8y/firmware.go
+++ pkg/c8y/firmware.go
@@ -77,7 +77,7 @@ func (s *InventoryFirmwareService) GetFirmwareByName(ctx context.Context, name s
 // GetFirmwareVersionsByName returns firmware package versions by name
 func (s *InventoryFirmwareService) GetFirmwareVersionsByName(ctx context.Context, firmwareID string, name string, paging *PaginationOptions) (*ManagedObjectCollection, *Response, error) {
 	opt := &ManagedObjectOptions{
-		Query:             fmt.Sprintf("(name eq '%s') and bygroupid(%s)", name, firmwareID),
+		Query:             fmt.Sprintf("(c8y_Firmware.version eq '%s') and bygroupid(%s)", name, firmwareID),
 		PaginationOptions: *paging,
 	}
 	return s.client.Inventory.GetManagedObjects(ctx, opt)
