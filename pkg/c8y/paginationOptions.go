f1bd077a6cf08f87df5be827d2a688168d300a89
reubenmiller <reuben.d.miller@gmail.com>
2022-10-03 21:39:37 +0200

    add support for withTotalElements in the statistics

--- pkg/c8y/paginationOptions.go
+++ pkg/c8y/paginationOptions.go
@@ -5,7 +5,11 @@ type PaginationOptions struct {
 	// Pagesize of results to return in one request
 	PageSize int `url:"pageSize,omitempty"`
 
+	// Include total pages included in the pagination at the given page size
 	WithTotalPages bool `url:"withTotalPages,omitempty"`
+
+	// Include count of elements in the statistics response. Only supported >= 10.13
+	WithTotalElements bool `url:"withTotalElements,omitempty"`
 }
 
 // NewPaginationOptions returns a pagination options object with a specified pagesize and WithTotalPages set to false
--- pkg/c8y/statistics.go
+++ pkg/c8y/statistics.go
@@ -2,9 +2,10 @@ package c8y
 
 // Statistics statistics about the returned rest response
 type Statistics struct {
-	CurrentPage *int `json:"currentPage"`
-	PageSize    *int `json:"pageSize"`
-	TotalPages  *int `json:"totalPages"`
+	CurrentPage   *int `json:"currentPage"`
+	PageSize      *int `json:"pageSize"`
+	TotalPages    *int `json:"totalPages"`
+	TotalElements *int `json:"totalElements"`
 }
 
 // BaseResponse common response from all c8y api calls
