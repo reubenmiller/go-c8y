ab02894c654fd63fa35942fd84d800382668160e
Reuben Miller <reuben.d.miller@gmail.com>
2024-04-21 17:24:13 +0200

    remove unused function

--- pkg/c8y/utils.go
+++ pkg/c8y/utils.go
@@ -12,73 +12,8 @@ import (
 	"strings"
 )
 
-func prepareMultipartRequest(method string, url string, values map[string]io.Reader) (req *http.Request, err error) {
-	// Prepare a form that you will submit to that URL.
-	var b bytes.Buffer
-	w := multipart.NewWriter(&b)
-
-	// Sort formdata keys
-	keys := make([]string, 0, len(values))
-	for key := range values {
-		keys = append(keys, key)
-	}
-	sort.Strings(keys)
-
-	for _, key := range keys {
-		r := values[key]
-		if key == "filename" {
-			// Ignore filename as it is used to name the uploaded file
-			continue
-		}
-
-		var fw io.Writer
-		if x, ok := r.(io.Closer); ok {
-			defer x.Close()
-		}
-		// Add an image file
-		if x, ok := r.(*os.File); ok {
-
-			// Check if manual filename field was provided, otherwise use the basename
-			filename := filepath.Base(x.Name())
-			if manual_filename, ok := values["filename"]; ok {
-				if b, rErr := io.ReadAll(manual_filename); rErr == nil {
-					filename = string(b)
-				} else {
-					err = rErr
-				}
-			}
-			if fw, err = w.CreateFormFile(key, filename); err != nil {
-				return
-			}
-		} else {
-			// Add other fields
-			if fw, err = w.CreateFormField(key); err != nil {
-				return
-			}
-		}
-		if _, err = io.Copy(fw, r); err != nil {
-			return nil, err
-		}
-
-	}
-	// Don't forget to close the multipart writer.
-	// If you don't close it, your request will be missing the terminating boundary.
-	w.Close()
-
-	// Now that you have a form, you can submit it to your handler.
-	req, err = http.NewRequest(method, url, &b)
-	if err != nil {
-		return
-	}
-	// Don't forget to set the content type, this will contain the boundary.
-	req.Header.Set("Content-Type", w.FormDataContentType())
-	req.Header.Set("Accept", "application/json")
-
-	return req, nil
-}
-
 // Prepare multipart form-data request which uses io.Pipe to buffer reading the message to ensure files won't be read entirely into memory
-func prepareMultipartRequestWithBuffer(method string, url string, values map[string]io.Reader) (req *http.Request, err error) {
+func prepareMultipartRequest(method string, url string, values map[string]io.Reader) (req *http.Request, err error) {
 	pr, pw := io.Pipe()
 
 	// Prepare a form that you will submit to that URL.
