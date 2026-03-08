239443113670eed1ba0530ac176279f1ab84fde0
reubenmiller <reuben.d.miller@gmail.com>
2026-03-08 13:40:14 +0100

    refactor: simplify retention rules test by using First helper

--- test/c8y_api_test/retention_rules_test.go
+++ test/c8y_api_test/retention_rules_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/reubenmiller/go-c8y/pkg/c8y/api/retentionrules"
 	"github.com/reubenmiller/go-c8y/test/c8y_api_test/testcore"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_RetentionRules(t *testing.T) {
@@ -17,14 +18,12 @@ func Test_RetentionRules(t *testing.T) {
 	assert.Greater(t, rules.Data.Length(), 0)
 
 	if rules.Data.Length() > 0 {
-		ruleID := ""
-		// TODO: Add a better iterator for retention rules, or have a method which returns a plain array
-		for item := range rules.Data.Iter() {
-			ruleID = item.Get("id").String()
-			break
-		}
-		rule := client.RetentionRules.Get(context.Background(), ruleID)
+
+		firstRule, err := rules.First()
+		require.NoError(t, err)
+
+		rule := client.RetentionRules.Get(context.Background(), firstRule.ID())
 		assert.NoError(t, rule.Err)
-		assert.Equal(t, rule.Data.ID(), ruleID)
+		assert.Equal(t, rule.Data.ID(), firstRule.ID())
 	}
 }
