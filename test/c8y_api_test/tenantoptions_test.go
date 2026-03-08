385a28ec37a17359e9cbbf0d13e21ade844fe29a
reubenmiller <reuben.d.miller@gmail.com>
2026-03-08 13:28:47 +0100

    refactor: simplify access to first tenant option in tests

--- test/c8y_api_test/tenantoptions_test.go
+++ test/c8y_api_test/tenantoptions_test.go
@@ -4,11 +4,10 @@ import (
 	"context"
 	"testing"
 
-	"github.com/reubenmiller/go-c8y/pkg/c8y/api/model"
 	"github.com/reubenmiller/go-c8y/pkg/c8y/api/tenants/tenantoptions"
-	"github.com/reubenmiller/go-c8y/pkg/c8y/jsondoc"
 	"github.com/reubenmiller/go-c8y/test/c8y_api_test/testcore"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_TenantOptions(t *testing.T) {
@@ -20,25 +19,21 @@ func Test_TenantOptions(t *testing.T) {
 	assert.NoError(t, collection.Err)
 	assert.Greater(t, collection.Data.Length(), 0)
 
-	// TODO: find a nicer way to access the first item in the results
-	var firstOption model.TenantOption
-	for item := range jsondoc.DecodeIter[model.TenantOption](collection.Data.Iter()) {
-		firstOption = *item
-		break
-	}
+	firstOption, err := collection.First()
+	require.NoError(t, err)
 
 	// get
 	option := client.Tenants.Options.Get(context.Background(), tenantoptions.GetOption{
-		Category: firstOption.Category,
-		Key:      firstOption.Key,
+		Category: firstOption.Category(),
+		Key:      firstOption.Key(),
 	})
 	assert.NoError(t, option.Err)
-	assert.Equal(t, option.Data.Category(), firstOption.Category)
-	assert.Equal(t, option.Data.Key(), firstOption.Key)
+	assert.Equal(t, option.Data.Category(), firstOption.Category())
+	assert.Equal(t, option.Data.Key(), firstOption.Key())
 
 	// list by category
 	category := client.Tenants.Options.ListByCategory(context.Background(), tenantoptions.ListByCategoryOptions{
-		Category: firstOption.Category,
+		Category: firstOption.Category(),
 	})
 	assert.NoError(t, category.Err)
 	assert.GreaterOrEqual(t, len(category.Data), 1)
